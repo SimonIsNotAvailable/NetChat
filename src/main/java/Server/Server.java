d12ea2b9ff5b64b1fb89e793fac95b1629620edb
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-12-12 12:51:55 +0500

    Server minor changes

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -3,14 +3,10 @@ package Server;
 import Logger.*;
 import Settings.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Server {
     private Logger logger = Logger.getInstance();
